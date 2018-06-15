0a010c70c11caeb2e59f0d5f94cdba7ea0ccc160
Jesus Rodriguez <arturo@ravelo.ca>
2018-06-14 18:51:59 -0700

    Ravelo update docs (#13)
    * adding docs for user model
    
    * update lib docs content

--- controllers/about.js
+++ controllers/about.js
@@ -5,9 +5,26 @@ const Config = require('../lib/config');
 
 let controllers = {};
 
+/**
+ * Definition of home API endpoint
+ * 
+ * @param  {} request
+ * @param  {} h
+ * @param  {Package.name} =>{return{name
+ * @param  {Package.version} version
+ * @param  {Config.env};}};} env
+ * @returns Config
+ */
 controllers.about = {
   method: 'GET',
   path: '/',
+
+  /**
+   * Controller handler that retrieves server information
+   * 
+   * @param  {} request HapiJS request object
+   * @param  {} h HapiJS response toolkit
+   */
   handler: (request, h) => {
     return {
       name: Package.name,
--- models/user.js
+++ models/user.js
@@ -2,10 +2,19 @@
 
 const mongoose = require('../init/mongoose_connection');
 
+/**
+ * @param  {String} email
+ */
 const Schema = new mongoose.Schema({
   email: String
 });
 
+/**
+ * User schema definition using monggoose
+ * 
+ * @param  {} 'User'
+ * @param  {} Schema
+ */
 const UserModel = mongoose.model('User', Schema);
 
 module.exports = UserModel;
